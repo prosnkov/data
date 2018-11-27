ca8ce63e24c1612a2a19acc0495865b2482ffd40
prosnkov <44735447+prosnkov@users.noreply.github.com>
2018-11-27 12:22:46 +0300

    Update sinp.py

--- sinp.py
+++ sinp.py
@@ -17,6 +17,19 @@ mpl.rc('text', usetex=True)
 mpl.rcdefaults()
 mpl.font = mpl.font_manager.FontProperties(fname="/usr/share/matplotlib/mpl-data/fonts/ttf/cmb10.ttf")
 
+
+elem=['neut','H','He','Li','Be','B','C','N','O','F','Ne','Na', \
+	'Mg','Al','Si','P','S','Cl','Ar','K','Ca','Sc','Ti', \
+	'V','Cr','Mn','Fe','Co','Ni','Cu','Zn','Ga','Ge', \
+	'As','Se','Br','Kr','Rb','Sr','Y','Zr','Nb','Mo','Tc', \
+	'Ru','Rh','Pd','Ag','Cd','In','Sn','Sb','Te','I','Xe', \
+	'Cs','Ba','La','Ce','Pr','Nd','Pm','Sm','Eu','Gd', \
+	'Tb','Dy','Ho','Er','Tm','Yb','Lu','Hf','Ta','W','Re', \
+	'Os','Ir','Pt','Au','Hg','Tl','Pb','Bi','Po','At', \
+	'Rn','Fr','Ra','Ac','Th','Pa','U','Np','Pu','Am','Cm', \
+	'Bk','Cf','Es','Fm','Md','No','Lr','Rf','Db','Sg', \
+	'Bh','Hs','Mt']
+
 #initialise talys, added Apr 2017
 def talys_init(element, mass, Emin, Emax, dE):
 #	d=os.getcwd() 
